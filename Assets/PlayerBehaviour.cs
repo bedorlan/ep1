de538b770bd351a0da1c0f63db88684ae15505c1
Orlando Bedoya Marin <omarin@twilio.com>
2020-03-14 14:15:48 -0500

    initial

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.touchSupported && Input.simulateMouseWithTouches)
+        {
+            Debug.Log("simulateMouseWithTouches=yes");
+        }
+        else
+        {
+            Debug.Log("simulateMouseWithTouches=no");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("touches!");
+        }
+    }
+}
