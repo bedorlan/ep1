e526d33ce160fcb2baef20e05331d9287f2c5fa2
Orlando Bedoya Marin <omarin@twilio.com>
2020-03-17 16:59:43 -0500

    adding female voters

---
+++ Assets/VoterBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VoterBehaviour : MonoBehaviour
+{
+    const char WOMAN_CODE = '\ue900';
+
+    void Start()
+    {
+        if (Random.value <= 0.5093)
+        {
+            GetComponent<TextMeshPro>().text = WOMAN_CODE.ToString();
+        }
+        
+    }
+}
