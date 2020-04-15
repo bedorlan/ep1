acb335aa862fa220e2f8cf77e95d8b9a7932e3c9
Orlando Bedoya Marin <omarin@twilio.com>
2020-04-15 18:43:50 -0500

    simple match results :)

--- Assets/Scripts/MatchResultBehaviour.cs
+++ Assets/Scripts/MatchResultBehaviour.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 internal class MatchResultBehaviour : MonoBehaviour
 {
+    public GameObject resultsObject;    
+
     internal event Action OnFinished;
 
     internal void ShowMatchResult(MatchResult matchResult)
     {
-        Debug.Log("ShowMatchResult");
+        var playerResultsOrdered = matchResult.playerResultsOrdered;
+        var playersCount = playerResultsOrdered.Count;
+
+        for (var i = 0; i < resultsObject.transform.childCount; ++i)
+        {
+            var active = i < playersCount;
+            var resultUI = resultsObject.transform.GetChild(i).gameObject;
+            resultUI.SetActive(active);
+            if (!active) continue;
+
+            var playerResult = playerResultsOrdered[i];
+            var tmPro = resultUI.GetComponent<TextMeshProUGUI>();
+            tmPro.text = string.Format("{0}{1} {2}", Common.MAN_CODE, Common.WOMAN_CODE, playerResult.votes);
+            tmPro.color = Common.playerColors[playerResult.playerNumber];
+        }
     }
 
     public void OnContine()
