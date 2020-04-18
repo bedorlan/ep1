33ae5535dea84804858d559bbe027458fa5a40a3
Orlando Bedoya Marin <omarin@twilio.com>
2020-04-18 12:48:05 -0500

    using buffer. perhaps is safer

--- Server/index.ts
+++ Server/index.ts
@@ -449,21 +449,17 @@ class TelepathyInputTransformer extends Transform {
   }
 
   private msgSize: number | undefined = undefined
-  private buffer = ''
+  private buffer = Buffer.from('')
 
   _transform(chunk: Buffer, encoding: string, cb: TransformCallback) {
-    this.buffer += chunk.toString()
+    this.buffer = Buffer.concat([this.buffer, chunk], this.buffer.length + chunk.length)
 
     while (true) {
       if (!this.msgSize) {
         if (this.buffer.length < 4) break
         const msg = this.buffer.slice(0, 4)
         this.buffer = this.buffer.slice(4)
-        this.msgSize =
-          msg.charCodeAt(0) * 256 ** 3 +
-          msg.charCodeAt(1) * 256 ** 2 +
-          msg.charCodeAt(2) * 256 ** 1 +
-          msg.charCodeAt(3) * 256 ** 0
+        this.msgSize = msg[0] * 256 ** 3 + msg[1] * 256 ** 2 + msg[2] * 256 ** 1 + msg[3] * 256 ** 0
       }
 
       if (this.buffer.length < this.msgSize) break
