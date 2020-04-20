42ea4391b1f88a3abfc320b6e016ba165d5eaec5
Orlando Bedoya Marin <omarin@twilio.com>
2020-04-20 14:56:07 -0500

    keep pressing to move player

--- Assets/Scripts/BackgroundBehaviour.cs
+++ Assets/Scripts/BackgroundBehaviour.cs
@@ -5,10 +5,20 @@ using UnityEngine.EventSystems;
 
 public class BackgroundBehaviour : MonoBehaviour
 {
+    private long mouseDownTime;
+
     private void OnMouseDown()
     {
+        mouseDownTime = Common.unixMillis();
         if (Common.IsPointerOverUIObject()) return;
+
         var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         NetworkManager.singleton.BackgroundClicked(position);
+    }
+
+    private void OnMouseDrag()
+    {
+        if (Common.unixMillis() - mouseDownTime < 500) return;
+        OnMouseDown();
     }
 }
